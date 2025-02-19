0a9c77fa98b531bdc4c822d6854afa2a51e17b1e
koki-maekawa <koki.study33@gmail.com>
2025-02-19 15:53:05 +0900

    #6 style: rubocop

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module App
 
     # 日本語に設定する
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
   end
 end
