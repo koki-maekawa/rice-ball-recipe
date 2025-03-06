b3efcb561878525a3d5deb11c1f31e675d15f092
koki-maekawa <koki.study33@gmail.com>
2025-03-06 14:25:11 +0900

    #11 style: format_erb

--- format_erb.sh
+++ format_erb.sh
@@ -2,6 +2,8 @@
 set -eo pipefail
 echo "Running HTML Beautifier..."
 bundle exec htmlbeautifier app/views/*/*.html.erb
+bundle exec htmlbeautifier app/views/*/*/*.html.erb
 
 echo "Running ERB Lint..."
 bundle exec erb_lint --autocorrect app/views/*/*.html.erb
+bundle exec erb_lint --autocorrect app/views/*/*/*.html.erb
