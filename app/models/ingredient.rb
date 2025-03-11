d9b199558a9c3189ddbeb50de611798337db6a71
koki-maekawa <koki.study33@gmail.com>
2025-03-11 12:19:26 +0900

    #18 feat: おにぎりレシピ一覧/詳細

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,12 @@ class Ingredient < ApplicationRecord
   belongs_to :rice_ball
 
   validates :name, :amount, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "name" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "rice_ball", "user" ]
+  end
 end
