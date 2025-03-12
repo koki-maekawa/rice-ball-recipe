c2d308725d7a64f1b1b6dbe3fa44acb2913b62b0
koki-maekawa <koki.study33@gmail.com>
2025-03-12 10:39:34 +0900

    #21 feat: bookmarksコントローラー作成

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,15 @@
+class BookmarksController < ApplicationController
+  def create
+    rice_ball = Rice_ball.find(params[:rice_ball_id])
+    bookmark = current_user.bookmarks.new(rice_ball_id: rice_ball.id)
+    bookmark.save
+    redirect_to request.referer
+  end
+  
+  def destroy
+    rice_ball = Rice_ball.find(params[:rice_ball_id])
+    bookmark = current_user.bookmarks.find_by(rice_ball_id: rice_ball.id)
+    bookmark.destroy
+    redirect_to request.referer
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  resources :rice_balls
+  resources :rice_balls do
+    resource :bookmarks, only: [ :create, :destroy ]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
