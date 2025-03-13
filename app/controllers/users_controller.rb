e24a89474eeadf2022e3152c4429be4a334b86a4
koki-maekawa <koki.study33@gmail.com>
2025-03-13 12:35:47 +0900

    #21 feat: ユーザー画面に遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
-    @created_rice_balls = current_user.rice_balls.page(params[:created_page]).per(4)
-    @bookmarked_rice_balls = current_user.bookmarked_rice_balls.page(params[:bookmarked_page]).per(4)
+    @user = User.find(params[:id])
+    @created_rice_balls = @user.rice_balls.page(params[:created_page]).per(4)
+    @bookmarked_rice_balls = @user.bookmarked_rice_balls.page(params[:bookmarked_page]).per(4)
     end
 end
