610e2a6044d101d8ca79f92ae898c9f8a36f9e63
koki-maekawa <koki.study33@gmail.com>
2025-03-13 12:36:53 +0900

    #21 feat: レシピ表示順序の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @created_rice_balls = @user.rice_balls.page(params[:created_page]).per(4)
-    @bookmarked_rice_balls = @user.bookmarked_rice_balls.page(params[:bookmarked_page]).per(4)
+    @created_rice_balls = @user.rice_balls.order(created_at: :desc).page(params[:created_page]).per(4)
+    @bookmarked_rice_balls = @user.bookmarked_rice_balls.order("bookmarks.created_at DESC").page(params[:bookmarked_page]).per(4)
     end
 end
