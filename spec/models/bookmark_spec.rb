afba8800bc111f175043092046e33ea441ade134
koki-maekawa <koki.study33@gmail.com>
2025-03-13 21:47:05 +0900

    #21 test: bookmark_modelSpec

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:rice_ball) { FactoryBot.create(:rice_ball) }
+
+  context 'バリデーション' do
+    it 'user_idとrice_ball_idの組み合わせが一意であること' do
+      FactoryBot.create(:bookmark, user: user, rice_ball: rice_ball)
+
+      duplicate_bookmark = Bookmark.new(user: user, rice_ball: rice_ball)
+      expect(duplicate_bookmark).to be_invalid
+    end
+  end
 end
