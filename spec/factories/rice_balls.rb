ebf3bea18d3625012ed21bce6a0930ad4a86d3b0
koki-maekawa <koki.study33@gmail.com>
2025-03-17 13:45:14 +0900

    #24 test: おにぎりレシピ編集/削除テスト

--- spec/factories/rice_balls.rb
+++ spec/factories/rice_balls.rb
@@ -7,5 +7,12 @@ FactoryBot.define do
     after(:build) do |rice_ball|
       rice_ball.image.attach(io: File.open('./spec/factories/test_image/onigiri.png'), filename: 'onigiri.png')
     end
+
+    trait :with_ingredients_and_steps do
+      after(:create) do |rice_ball|
+        create_list(:ingredient, 2, rice_ball: rice_ball)
+        create_list(:step, 2, rice_ball: rice_ball)
+      end
+    end
   end
 end
