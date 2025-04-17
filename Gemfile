97421632455f035eeb6f7318f1ae21bf091632c6
koki-maekawa <koki.study33@gmail.com>
2025-04-17 14:17:20 +0000

    #45 chore: playwrightに変更

--- Gemfile
+++ Gemfile
@@ -82,6 +82,6 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
-  gem "selenium-webdriver"
+  gem "capybara-playwright-driver"
   gem "faker"
 end
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,16 +1,11 @@
 require 'capybara/rspec'
-require 'selenium-webdriver'
 
-Capybara.register_driver :remote_chrome do |app|
-    url = ENV["SELENIUM_DRIVER_URL"]
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('headless')
-    options.add_argument('window-size=1680x1050')
-
-    Capybara::Selenium::Driver.new(app,
-                                browser: :remote,
-                                url: url,
-                                options: options)
+Capybara.register_driver :my_playwright do |app|
+    Capybara::Playwright::Driver.new(app,
+        browser_type: ENV["PLAYWRIGHT_BROWSER"]&.to_sym || :chromium,
+        headless: (false unless ENV["CI"] || ENV["PLAYWRIGHT_HEADLESS"]),
+        viewport: { width: 900, height: 1800 }
+    )
 end
 
 RSpec.configure do |config|
@@ -19,13 +14,6 @@ RSpec.configure do |config|
     end
 
     config.before(:each, :js, type: :system) do
-        if ENV['SELENIUM_DRIVER_URL']
-            driven_by :remote_chrome
-            Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-            Capybara.server_port = 4444
-            Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-        else
-            driven_by :selenium_chrome_headless
-        end
+        driven_by(:my_playwright)
     end
 end
