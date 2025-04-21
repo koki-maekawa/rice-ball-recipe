2bdb57d3a46fb44b60dab481b287175558fa517a
koki-maekawa <koki.study33@gmail.com>
2025-04-21 02:18:39 +0000

    #49 fix: ユーザー画面のおにぎりレシピ表示数変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show created_index bookmarked_index]
 
   def show
-    @created_rice_balls = @user.rice_balls.includes(:image_attachment).order(created_at: :desc).limit(2)
+    @created_rice_balls = @user.rice_balls.includes(:image_attachment).order(created_at: :desc).limit(4)
     @created_rice_balls_count = @user.rice_balls.size
-    @bookmarked_rice_balls = @user.bookmarked_rice_balls.includes(:image_attachment, :user).order("bookmarks.created_at DESC").limit(2)
+    @bookmarked_rice_balls = @user.bookmarked_rice_balls.includes(:image_attachment, :user).order("bookmarks.created_at DESC").limit(4)
     @bookmarked_rice_balls_count = @user.bookmarked_rice_balls.size
   end
 
