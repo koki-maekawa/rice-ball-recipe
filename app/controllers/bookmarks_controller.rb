c9c21ef69d5212c4591978419d1a08b868ea0226
koki-maekawa <koki.study33@gmail.com>
2025-06-27 06:46:37 +0000

    #133 fix: turbo_frameからturbo_streamに変更

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,13 +3,13 @@ class BookmarksController < ApplicationController
     rice_ball = RiceBall.find(params[:rice_ball_id])
     bookmark = current_user.bookmarks.new(rice_ball_id: rice_ball.id)
     bookmark.save
-    redirect_to request.referer
+    render turbo_stream: turbo_stream.update("bookmark_rice_ball_#{rice_ball.id}", partial: "shared/bookmark", locals: { rice_ball: rice_ball, size: params[:size] })
   end
 
   def destroy
     rice_ball = RiceBall.find(params[:rice_ball_id])
     bookmark = current_user.bookmarks.find_by(rice_ball_id: rice_ball.id)
     bookmark.destroy
-    redirect_to request.referer
+    render turbo_stream: turbo_stream.update("bookmark_rice_ball_#{rice_ball.id}", partial: "shared/bookmark", locals: { rice_ball: rice_ball, size: params[:size] })
   end
 end
