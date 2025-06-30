f39c7ea43624398e73242574946a2d9cdf3ef04c
koki-maekawa <koki.study33@gmail.com>
2025-06-30 02:42:06 +0000

    #135 feat: from_omniauthメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,14 @@ class User < ApplicationRecord
   def already_bookmarked?(rice_ball)
     bookmarks.pluck(:rice_ball_id).include?(rice_ball.id)
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.nickname || auth.info.name
+      user.email = auth.info.email || "#{auth.uid}@twitter.local"
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
