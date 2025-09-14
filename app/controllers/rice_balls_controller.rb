5c601c17cff29d3d37c2711201ca9e573ff594c6
koki-maekawa <koki.study33@gmail.com>
2025-09-14 04:25:13 +0000

    #139 feat: タグ作成処理

--- app/controllers/rice_balls_controller.rb
+++ app/controllers/rice_balls_controller.rb
@@ -68,9 +68,11 @@ class RiceBallsController < ApplicationController
     end
 
     def form_params
-      params.require(:recipe_form).permit(rice_ball_attributes: %i[title description image],
-                                       ingredients_attributes: %i[id name amount _destroy],
-                                       steps_attributes: %i[id description step_number _destroy],
-                                       )
+      params.require(:recipe_form).permit(
+        :tag_names,
+        rice_ball_attributes: %i[title description image],
+        ingredients_attributes: %i[id name amount _destroy],
+        steps_attributes: %i[id description step_number _destroy]
+      )
     end
 end
